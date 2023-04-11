ced3986d128996b6997dd65e91037ea5539fd74e
KirthikaKS <127963062+KirthikaKS@users.noreply.github.com>
2023-04-11 21:47:26 +0530

    Add files via upload

---
+++ SQLQuery3_SELECT and FROM.sql
@@ -0,0 +1,23 @@
+SELECT*
+FROM EmployeeDemographics
+
+SELECT FirstName, Gender FROM EmployeeDemographics
+
+SELECT TOP 6 *
+FROM EmployeeSalary
+
+SELECT DISTINCT(EmployeeID)
+FROM EmployeeDemographics
+
+SELECT DISTINCT (Salary)
+FROM EmployeeSalary
+
+SELECT MAX(Salary)
+FROM EmployeeSalary
+
+SELECT MIN(Salary)
+FROM EmployeeSalary
+
+SELECT AVG(SALARY)
+FROM EmployeeSalary
+
